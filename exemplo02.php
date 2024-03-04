4add7ead0e9d061da03b8ae92af343975b67f8d9
augustooxz <lucass.aaugustoo@gmail.com>
2024-03-04 20:23:43 -0300

    exemplo 02 php

---
+++ exemplo02.php
@@ -0,0 +1,19 @@
+<?php 
+
+	$valor1 = 40.50;
+	//variável 
+	$valor2 = 30;
+	$nome = "Etecia";
+	$cidade = 'Corinthians';
+	$flag = true;
+	$status = false;
+
+	
+    echo "$valor1 <br />";
+    echo "$valor2 <br />";
+    echo "$nome <br />";
+    echo "$cidade <br />";
+    echo "$flag <br />";
+    echo "$status <br />";
+
+ ?>
