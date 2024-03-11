585a556c6bb13705fa971e88cc55adfb4ea01a20
augustooxz <lucass.aaugustoo@gmail.com>
2024-03-11 20:16:39 -0300

    exemplo com escopo de função

---
+++ exemplo05.php
@@ -0,0 +1,18 @@
+<?php 
+
+	$nome = "Cássio" ;
+
+ function exibir(){
+	$nome = "Carlos Miguel";
+		echo "Valor da variável dentro da função " . $nome;
+
+}
+
+exibir();
+
+		echo "<br> Valor da variável fora da função " . $nome;
+
+
+
+
+ ?>
