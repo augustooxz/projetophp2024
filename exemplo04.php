c08666499db26df6aea31560d1e20e247b6bc6f9
augustooxz <lucass.aaugustoo@gmail.com>
2024-03-11 19:49:29 -0300

    projetophp2024

---
+++ exemplo04.php
@@ -0,0 +1,19 @@
+<?php 
+
+$valor1 = 50.00;
+$valor2 = 30.50;
+
+
+$soma = $valor1 + $valor2;
+
+echo "A soma da $valor1 mais $valor2 é $soma <br> ";
+
+
+echo 'A soma de ' . $valor1 . ' mais ' . $valor2 . ' é ' . $soma . '<br>';
+
+
+
+
+
+
+ ?>
