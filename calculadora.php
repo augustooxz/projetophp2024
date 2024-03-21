46f1e7cdd559b0245f04498dcad73e50b283df66
augustooxz <lucass.aaugustoo@gmail.com>
2024-03-20 22:11:35 -0300

    criando funcoes

---
+++ calculadora.php
@@ -0,0 +1,23 @@
+<?php 
+	
+	$num1 = 30.5;
+	$num2 = 20.5;
+
+	function somar($num1 , $num2){
+		return $num1 + $num2 ;
+}
+
+	function subtracao($num1 , $num2){
+		return $num1 - $num2 ;
+}
+
+	function divisao($num1 , $num2){
+		return $num1 / $num2 ;
+}
+
+	function multiplicacao($num1 , $num2){
+		return $num1 * $num2 ;
+}
+
+
+?>
---
+++ exemplo09.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+function calcular ($num1, $num2){
+
+return (30.5 + 20.5);
+
+}
+
+echo calcular($num1, $num2);
+
+;
+
+
+
+
+
+ ?>
